f4c05b0a9030bff5d70f777fe836138ca79da21f
SeongGyu <junsung3807@naver.com>
2024-10-31 23:49:19 +0900

    refactor: Create multiplyOperation class inherited from calculator class.

--- NBC_Calculator.playground/Contents.swift
+++ NBC_Calculator.playground/Contents.swift
@@ -6,25 +6,6 @@ enum Operator {
     case mul
     case div(secondNumber: Double, description: String)
     case rem
-    case none(description: String)
-    
-    
-    init?(rawValue: String) {
-        switch rawValue {
-        case "+":
-            self = .add
-        case "-":
-            self = .sub
-        case "*":
-            self = .mul
-        case "/":
-            self = .div(secondNumber: 0.0, description: "It cannot be divided by zero.")
-        case "%":
-            self = .rem
-        default:
-            self = .none(description: "This operator is not supported.")
-        }
-    }
 }
 
 class Calculator {
@@ -52,10 +33,19 @@ final class SubtractOperation: Calculator {
     func calculate(firstNumber: Double, secondNumber: Double) -> String { setNumberFormatter(number: firstNumber - secondNumber) }
 }
 
+final class MultiplyOperation: Calculator {
+    init() { super.init(operatorCase: .mul) }
+    
+    func calculate(firstNumber: Double, secondNumber: Double) -> String { setNumberFormatter(number: firstNumber * secondNumber) }
+}
+
 let addCalculator = AddOperation()
 let subCalculator = SubtractOperation()
+let mulCalculator = MultiplyOperation()
 let addResult = addCalculator.calculate(firstNumber: 100, secondNumber: -333333330.011)
 let subResult = subCalculator.calculate(firstNumber: -100.2, secondNumber: -100.1)
+let mulResult = mulCalculator.calculate(firstNumber: 0.2, secondNumber: -300.0)
 
 print(addResult)
 print(subResult)
+print(mulResult)
