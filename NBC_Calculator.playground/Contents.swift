b9f9a5425d12c480194988b6743f33293cbdb97e
SeongGyu <junsung3807@naver.com>
2024-10-31 21:48:02 +0900

    refactor: Set calculator class to superclasee.

--- NBC_Calculator.playground/Contents.swift
+++ NBC_Calculator.playground/Contents.swift
@@ -28,36 +28,42 @@ enum Operator {
 }
 
 class Calculator {
+    let operatorCase: Operator
+    
+    init(operatorCase: Operator) {
+        self.operatorCase = operatorCase
+    }
+    
     private func setNumberFormatter(number: Double) -> String {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
         
         return numberFormatter.string(for: number) ?? "0.0"
     }
-    
-    func calculate(operator: String, firstNumber: Double, secondNumber: Double) -> String {
-        let resultValue: Double
-        guard let op = Operator(rawValue: `operator`) else { fatalError() }
-        
-        switch op {
-        case .add:
-            resultValue = firstNumber + secondNumber
-        case .sub:
-            resultValue = firstNumber - secondNumber
-        case .mul:
-            resultValue = firstNumber * secondNumber
-        case .div(secondNumber, let description) where secondNumber == 0:
-            return description
-        case .div(_, _):
-            resultValue = firstNumber / secondNumber
-        case .rem:
-            resultValue = firstNumber.truncatingRemainder(dividingBy: secondNumber)
-        case .none(let description):
-            return description
-        }
-        
-        return setNumberFormatter(number: resultValue)
-    }
+//
+//    func calculate(operator: String, firstNumber: Double, secondNumber: Double) -> String {
+//        let resultValue: Double
+//        guard let op = Operator(rawValue: `operator`) else { fatalError() }
+//        
+//        switch op {
+//        case .add:
+//            resultValue = firstNumber + secondNumber
+//        case .sub:
+//            resultValue = firstNumber - secondNumber
+//        case .mul:
+//            resultValue = firstNumber * secondNumber
+//        case .div(secondNumber, let description) where secondNumber == 0:
+//            return description
+//        case .div(_, _):
+//            resultValue = firstNumber / secondNumber
+//        case .rem:
+//            resultValue = firstNumber.truncatingRemainder(dividingBy: secondNumber)
+//        case .none(let description):
+//            return description
+//        }
+//        
+//        return setNumberFormatter(number: resultValue)
+//    }
 }
 
 final class AddOperation: Calculator {
@@ -76,17 +82,17 @@ final class DivideOperation: Calculator {
     
 }
 
-let calculator = Calculator()
-let noneCaseResult = calculator.calculate(operator: "&", firstNumber: 10, secondNumber: 10)
-let addResult = calculator.calculate(operator: "+", firstNumber: 11234, secondNumber: 12342)
-let subResult = calculator.calculate(operator: "-", firstNumber: 1000000, secondNumber: -19)
-let mulResult = calculator.calculate(operator: "*", firstNumber: 23.33, secondNumber: 0.9)
-let divResult = calculator.calculate(operator: "/", firstNumber: 7, secondNumber: 0)
-let remResult = calculator.calculate(operator: "%", firstNumber: 7, secondNumber: 9)
-
-print(noneCaseResult)
-print("Add result: \(addResult)")
-print("Subtract result: \(subResult)")
-print("Multiply result: \(mulResult)")
-print("Divide result: \(divResult)")
-print("Remaining result: \(remResult)")
+//let calculator = Calculator()
+//let noneCaseResult = calculator.calculate(operator: "&", firstNumber: 10, secondNumber: 10)
+//let addResult = calculator.calculate(operator: "+", firstNumber: 11234, secondNumber: 12342)
+//let subResult = calculator.calculate(operator: "-", firstNumber: 1000000, secondNumber: -19)
+//let mulResult = calculator.calculate(operator: "*", firstNumber: 23.33, secondNumber: 0.9)
+//let divResult = calculator.calculate(operator: "/", firstNumber: 7, secondNumber: 0)
+//let remResult = calculator.calculate(operator: "%", firstNumber: 7, secondNumber: 9)
+//
+//print(noneCaseResult)
+//print("Add result: \(addResult)")
+//print("Subtract result: \(subResult)")
+//print("Multiply result: \(mulResult)")
+//print("Divide result: \(divResult)")
+//print("Remaining result: \(remResult)")
