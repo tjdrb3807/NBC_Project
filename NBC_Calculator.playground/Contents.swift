9863bc3ec23baddeaa7f261ae47098b2aa777bfb
SeongGyu <junsung3807@naver.com>
2024-11-01 01:31:01 +0900

    comment: Add comment.

--- NBC_Calculator.playground/Contents.swift
+++ NBC_Calculator.playground/Contents.swift
@@ -9,6 +9,9 @@ enum Operator {
 }
 
 protocol AbstractFormatter {
+    /// 숫자에 10진법 표기하기 위해 NumberFormatter를 설정하는 함수
+    /// - Parameter number: 10진법으로 표기할 Double 타입의 수
+    /// - Returns: 변환한 10진법 수를 String 타입으로 반환
     func setNumberFormatter(number: Double) -> String
 }
 
@@ -16,14 +19,20 @@ final class Formatter: AbstractFormatter {
     func setNumberFormatter(number: Double) -> String {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 5
         
-        return numberFormatter.string(for: number) ?? "0.0"
+        return numberFormatter.string(for: number) ?? ""
     }
 }
 
 protocol AbstractOperation {
     var formatter: AbstractFormatter { get }
     
+    /// 프로토콜을 준수하는 클래스의 종류에 따라 두 수를 사칙연산하는 함수
+    /// - Parameters:
+    ///   - firstNumber: 1항에 해당하는 매개변수
+    ///   - secondNumber: 2항에 해당하는 매개변수
+    /// - Returns: 사칙연산 결과값에 십진법 표기하기 및 예외 사항시 사용자에게 내용을 전달하기 위해 String 타입 반환
     func calculate(firstNumber: Double, secondNumber: Double) -> String
 }
 
@@ -61,7 +70,15 @@ final class SubtractOperation: AbstractOperation {
         self.formatter = formatter
     }
     
-    func calculate(firstNumber: Double, secondNumber: Double) -> String { formatter.setNumberFormatter(number: firstNumber - secondNumber) }
+    func calculate(firstNumber: Double, secondNumber: Double) -> String {
+        /*
+            두 번째 항에 음수가 들어오면 add 연산이 되므로 클래스의도와 맞지 않다 판단.
+            음수 입력시 양수 입력 안내문구 반환
+         */
+        guard secondNumber >= 0 else { return "Please enter a positive number." }
+        
+        return formatter.setNumberFormatter(number: firstNumber - secondNumber)
+    }
 }
 
 final class MultiplyOperation: AbstractOperation {
@@ -82,6 +99,7 @@ final class DivideOperation: AbstractOperation {
     }
     
     func calculate(firstNumber: Double, secondNumber: Double) -> String {
+        // 사용자가 0으로 나누려고 시도하면 불가능 안내 문구 반환
         guard secondNumber != 0 else { return "It cannot be divided by zero." }
         
         return formatter.setNumberFormatter(number: firstNumber / secondNumber)
@@ -101,7 +119,7 @@ let calculator = Calculator(
     mulCalculator: mutCalculator,
     divCalculator: divCalculator)
 
-print(calculator.addCalculator.calculate(firstNumber: 10, secondNumber: 10))
-print(calculator.subCalculator.calculate(firstNumber: 10, secondNumber: 10))
-print(calculator.mulCalculator.calculate(firstNumber: 10, secondNumber: 10))
+print(calculator.addCalculator.calculate(firstNumber: 12341234, secondNumber: 10.99999))
+print(calculator.subCalculator.calculate(firstNumber: 123, secondNumber: -12345))
+print(calculator.mulCalculator.calculate(firstNumber: 7, secondNumber: 3.2))
 print(calculator.divCalculator.calculate(firstNumber: 10, secondNumber: 0))
