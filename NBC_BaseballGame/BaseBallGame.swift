e49271862c2b50550c035e1f100f4a893ca78215
Jeon_Seong_gyu <junsung3807@naver.com>
2024-11-07 19:02:23 +0900

    💡 Add comments

--- NBC_BaseballGame/BaseBallGame.swift
+++ NBC_BaseballGame/BaseBallGame.swift
@@ -39,22 +39,29 @@ final class BaseBallGame {
         print("< 숫자 야구 게임을 종료합니다 >")
     }
     
+    
+    /// 랜덤한 숫자 3개로 이루어진 배열 생성 메서드
+    /// - answer 파라미터에 할당
     private func makeAnswer() {
-        var isFirst = true
+        var isFirst = true  // 첫 번째 배열에 0을 넣지 않기 위한 flag
         
         while answer.count < 3 {
             guard !isFirst else {
-                answer.append(Int.random(in: 1...9))
+                answer.append(Int.random(in: 1...9)) // 첫 번째 배열에 0이 담길 수 없도록 범위 조정
                 isFirst = false
                 
                 continue
             }
             
             let number = Int.random(in: 0...9)
-            if !answer.contains(number) { answer.append(number) }
+            if !answer.contains(number) { answer.append(number) }   // 중복 방지 코드
         }
     }
     
+    
+    /// 사용자 입력 요청 메서드
+    ///  - 정상 입력시 enteredAnswer 파라미터에 할당
+    /// - Returns: 잘못된 입력값을 받았을 경우 예외처리를 위한 Enum 객체 반환
     private func requestAnswer() -> InvalidAnswer? {
         enteredAnswer.removeAll()
         print("숫자를 입력해주세요")
@@ -72,6 +79,8 @@ final class BaseBallGame {
         return nil
     }
     
+    /// 안내문구 및 Option 선택 가이드 호출 메서드
+    /// - Option 선택시 잘못된 번호를 입력하면 InvalidOption.rawValue 호출
     private func startIntro() {
         print("환영합니다! 원하시는 번호를 입력해주세요.")
         
@@ -107,8 +116,8 @@ final class BaseBallGame {
         print("\n< 게임을 시작합니다 >")
         makeAnswer()
         repository.add()
-        guard let lastIndex = repository.dataBase.last else { return }
-        print(answer)
+        guard let lastIndex = repository.dataBase.last else { return }  // 해당 게임 데이터 저장소 위치
+//        print(answer)
         
         while true {
             invalidAnswer = requestAnswer()
@@ -148,6 +157,8 @@ final class BaseBallGame {
         }
     }
     
+    /// enteredAnswer 배열에 담긴 요소와 answer 배열에 담긴 요소 비교 메서드
+    /// - Returns: 각 경우의 수에 해당하는 Hint 객체를 연관값과 함께 반환
     private func startInning() -> Hint {
         var strike = 0
         var ball = 0
@@ -169,9 +180,14 @@ final class BaseBallGame {
 }
 
 extension BaseBallGame {
+    
+    /// 배열 내 중복 확인 메서드
+    /// - Parameter arr: 정렬되지 않은 배열
+    /// - Returns: true - 중복 존재, false - 중복 없음
     func isDuplicate(_ arr: String) -> Bool {
-        let sortedArr = arr.sorted(by: >)
+        let sortedArr = arr.sorted(by: >)   // 오름차순으로 정렬
         
+        // 정렬된 배열에서 현제 인덱스에 위치한 요소와 다음 인덱스에 위치한 요소가 같으면 중복
         for i in sortedArr.startIndex..<sortedArr.endIndex - 1 where sortedArr[i] == sortedArr[i + 1] { return true }
         
         return false
--- NBC_BaseballGame/Repository.swift
+++ NBC_BaseballGame/Repository.swift
@@ -7,17 +7,21 @@
 
 import Foundation
 
+/// 다른 객체에서 참조를 통해 엑세스 할 수 있도록 Class로 구현
 class GameRecord {
     var challengeCount = 0
     
+    /// 1회 시도시 해당 라운드의 게임 시도 횟수 1 증가
     func updateChallengeCount() {
         self.challengeCount += 1
     }
 }
 
 protocol GameRepository {
+    /// GameRecord 저장소
     var dataBase: [GameRecord] { get set }
     
+    /// 게임 시작과 동시에 저장소(dataBase)에 해당 게임 데이터(GameRecord) 저장
     func add()
     
     func show()
