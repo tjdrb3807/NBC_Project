2534a20b5241e47cad09fd2ef598962aea92bdd8
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-09 16:18:34 +0900

    🎉 Begin a project

---
+++ NBC_PhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBC_PhoneBook
+//
+//  Created by 전성규 on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
